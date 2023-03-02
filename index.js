c0ff2005833e964bba99923b89e4e2ed35f323d2
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-02 16:48:29 -0600

    bug fixes

--- index.js
+++ index.js
@@ -102,92 +102,149 @@ function updateEmpRole() {
 }
 
 function addEmp() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'first_name',
-      message: 'Enter employee first name:'
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: 'Enter employee last name:'
-    },
-    {
-      type: 'input',
-      name: 'role_id',
-      message: 'Enter employee role ID:'
-    },
-    {
-      type: 'input',
-      name: 'manager_id',
-      message: 'Enter employee manager ID:'
+  let managerList = [];
+  let roleList = [];
+  db.query(
+    "SELECT * FROM employee where manager_id is null;",
+    (err, results) => {
+      if (err) throw err;
+      managerList = results.map((employee) => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id,
+      }));
+      db.query("select * from role;", (err, results) => {
+        if (err) throw err;
+        roleList = results.map((role) => ({
+          name: role.title,
+          value: role.id,
+        }));
+
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "first_name",
+              message: "Enter employee first name:",
+            },
+            {
+              type: "input",
+              name: "last_name",
+              message: "Enter employee last name:",
+            },
+            {
+              type: "list",
+              name: "role_id",
+              message: "Enter employee role ID:",
+              choices: roleList,
+            },
+            {
+              type: "list",
+              name: "manager_id",
+              message: "Enter employee manager ID:",
+              choices: managerList,
+            },
+          ])
+          .then((answers) => {
+            const { first_name, last_name, role_id, manager_id } = answers;
+            const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+            db.query(
+              sql,
+              [first_name, last_name, role_id, manager_id],
+              (error, results, fields) => {
+                if (error) console.error(error);
+                return;
+              }
+            );
+            console.log("Employee added successfully!");
+            initialQ();
+          });
+      });
     }
-]).then(answers => {
-    const { first_name, last_name, role_id, manager_id } = answers;
-    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-    db.query(sql, [first_name, last_name, role_id, manager_id], (error, results, fields) => {
-      if (error) 
-        console.error(error);
-        return;})
-        console.log('Employee added successfully!');
-        connection.end();
+  );
+}
 
+function addDept() {
+  inquirer
+    .prompt({
+      type: "input",
+      name: "name",
+      message: "What is the name of the department?",
+    })
+    .then(({ name }) => {
+      // Build the SQL insert statement
+      const sql = "INSERT INTO department SET ?";
 
-    })};
+      // Execute the insert statement with the provided parameters
+      db.query(sql, { name }, (error, results, fields) => {
+        if (error) {
+          // Display an error message to the user if the insert statement fails
+          console.error(error);
+          console.log("Failed to add department");
+        } else {
+          // Display a success message to the user if the insert statement succeeds
+          console.log(`Added department ${name}`);
+          initialQ();
+        }
+      });
+    });
+}
+function addRole() {
+  let departmentList = [];
 
-    function addDept() {
-      inquirer.prompt({
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the department?'
-      }).then(({ name }) => {
-        // Create a database connection
-        const connection = mysql.createConnection({
-          host: 'localhost',
-          user: 'username',
-          password: 'password',
-          database: 'employee_db'
-        });
-        
+  db.query("SELECT * FROM department ;", (err, results) => {
+    if (err) throw err;
+    departmentList = results.map((department) => ({
+      name: `${department.name} department`,
+      value: department.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary?",
+        },
+        {
+          type: "list",
+          name: "department",
+          choices: departmentList,
+          message: "What is the department?",
+        }
+       ] )
+      .then(({ title, salary, department }) => {
         // Build the SQL insert statement
-        const sql = 'INSERT INTO department SET ?';
-        
+        const sql =
+          "insert into role(title,salary,department_id)values (?,?,?);";
+
         // Execute the insert statement with the provided parameters
-        connection.query(sql, { name }, (error, results, fields) => {
+        db.query(sql, [title, salary, department], (error, results, fields) => {
           if (error) {
             // Display an error message to the user if the insert statement fails
             console.error(error);
-            console.log('Failed to add department');
+            console.log("Failed to add role");
           } else {
             // Display a success message to the user if the insert statement succeeds
-            console.log(`Added department ${name}`);
+            console.log(`Added role ${title}}`);
+            initialQ();
           }
-          
-          // Close the database connection
-          connection.end();
         });
       });
-    }    
-      
-    function selectAllEmp() {
-        db.query("SELECT * FROM employee", function (err, results) {
-          if (err) throw err;
-          console.table(results);
-          initialQ();
-        });
-      }    
-
-function selectAllRoles() {
-  db.query("SELECT * FROM role", function (err, results) {
+  });
+}
+function selectAllEmp() {
+  db.query("SELECT * FROM employee", function (err, results) {
     if (err) throw err;
     console.table(results);
     initialQ();
   });
-
-  
 }
-function addRole() {
+
+function selectAllRoles() {
   db.query("SELECT * FROM role", function (err, results) {
     if (err) throw err;
     console.table(results);
@@ -200,7 +257,7 @@ function selectAllDept() {
     console.table(results);
     initialQ();
   });
-}      
+}
 
 // const inquirer = require("inquirer");
 // const mysql = require("mysql2");
@@ -337,9 +394,9 @@ function selectAllDept() {
 //     const name = answers.name;
 //     const salary = answers.salary;
 //     const department = answers.department;
-  
+
 //     const sql = `INSERT INTO employee (name, salary, department) VALUES ('${name}', ${salary}, '${department}')`;
-  
+
 //     connection.query(sql, (error, results, fields) => {
 //       if (error) {
 //         console.error(error);
@@ -350,7 +407,7 @@ function selectAllDept() {
 //     });
 //   });
 // };
-  
+
 // function selectAllEmp() {
 //   db.query("SELECT * FROM employee", function (err, results) {
 //     if (err) throw err;
@@ -366,7 +423,6 @@ function selectAllDept() {
 //     initialQ();
 //   });
 
-  
 // }
 // function addRole() {
 //   db.query("SELECT * FROM role", function (err, results) {
@@ -395,7 +451,7 @@ function selectAllDept() {
 //     password : 'rootroot',
 //     database : 'yourdatabase'
 //   });
-  
+
 //   connection.connect((error) => {
 //     if (error) {
 //       console.error('Error connecting to database: ', error);
@@ -403,7 +459,7 @@ function selectAllDept() {
 //     }
 //     console.log('Connected to database!');
 //   });
-  
+
 //   // Now you can use the "connection" object to query the database
 //   connection.query(sql, (error, results, fields) => {
 //     if (error) {
@@ -411,7 +467,7 @@ function selectAllDept() {
 //       return;
 //     }
 //     console.log('Query results: ', results);
-//   }); 
+//   });
 
 //   inquirer.prompt([
 //     {
